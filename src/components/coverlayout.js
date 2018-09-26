06d232408fe635036e2932dc9ca5e6f5d4fd31c6
Wattanit Hotrakool <wattanit@eatlab.space>
2018-09-26 16:43:12 +0700

    Add transitional background images to cover

--- src/components/coverlayout.js
+++ src/components/coverlayout.js
@@ -3,20 +3,64 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Layout from './layout'
-import './layout.css'
+import "./style.css"
 
-const CoverPage = ({ children }) => (
-  <div style={{
-    backgroundColor: 'blue',
-    width: '100vw',
-    height: '100vh',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    padding: '10%'
-  }}>
-    {children}
-  </div>
-)
+import cover1 from '../images/cover-1.jpg'
+import cover2 from '../images/cover-2.jpg'
+import cover3 from '../images/cover-3.jpg'
+
+const coverDelayTime = 6000
+
+class CoverPage extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      theme: 0,
+      limit: 2
+    }
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(()=>{
+      let theme = this.state.theme+1
+      if (theme > this.state.limit){
+        theme = 0
+      }
+      this.setState({
+        theme: theme
+      })
+    }, coverDelayTime)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+
+
+  render(){
+    let uri
+    let color
+    if(this.state.theme===0){
+      uri = cover1
+      color = "white"
+    }else if (this.state.theme===1) {
+      uri = cover2
+      color = "white"
+    }else{
+      uri = cover3
+      color = "white"
+    }
+
+    return (
+      <div className="coverImage" style={{
+        backgroundImage: "url("+uri+")",
+        color: color
+      }}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 export default CoverPage
