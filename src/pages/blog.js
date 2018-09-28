ed582bf6070a50c4bcc4818efc2083b69791c6d0
Wattanit Hotrakool <wattanit@eatlab.space>
2018-09-28 17:58:06 +0700

    Fix redirection bug

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,12 +3,17 @@ import React from 'react'
 import ArticlePage from '../components/articlelayout'
 
 const Blog = ()=>{
-  window.location.href = "http://rorasa.wordpress.com"
   return (
     <ArticlePage
       title="Blog"
       backgroundColor="black"
       textColor="white">
+      <h2>Rorasa's Blog</h2>
+
+      <p>
+        An old blog of Wattanit can be found at
+        <a href="http://rorasa.wordpress.com" target="_blank">http://rorasa.wordpress.com</a>
+      </p>
     </ArticlePage>
   )
 }
